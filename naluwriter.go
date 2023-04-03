3fd96ad0e8797d70189053871709993ec5c0696d
陈恒捷 <chenhengjie@lizhi.fm>
2023-04-03 18:00:18 +0800

    docs: 关闭nanowriter的日志打印, 避免日志过多

--- naluwriter.go
+++ naluwriter.go
@@ -3,7 +3,7 @@ package main
 import (
 	"encoding/binary"
 	"github.com/chenhengjie123/quicktime_video_hack/screencapture/coremedia"
-	log "github.com/sirupsen/logrus"
+	// log "github.com/sirupsen/logrus"
 )
 
 var startCode = []byte{00, 00, 00, 01}
@@ -62,7 +62,7 @@ func (nw NaluWriter) writeNalu(bytes []byte) error {
 		return nil
 	}
 	if len(bytes) > 0 {
-		log.Debug("Send bytes " + string(startCode) + " with length: ", len(bytes))
+		// log.Debug("Send bytes " + string(startCode) + " with length: ", len(bytes))
 		nw.receiver.send <- append(startCode, bytes...)
 	}
 	return nil
